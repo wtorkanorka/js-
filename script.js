b6483cb85cc3414311d079b36188dbf3281f1600
Сергей Щуклин <wtorkanorka.shuclin@yandex.ru>
2022-01-27 17:26:33 +0500

    js

---
+++ script.js
@@ -0,0 +1,56 @@
+//сделать первый символь заглавным///////////////
+
+// function ucFirst(str) {
+
+//   let newStr = str[0].toUpperCase();
+//   alert(str);
+// }
+
+// alert(ucFirst("вася"));
+
+// не работает
+
+////////////////////////////////////////////////
+
+//проверка на спам//////////////////////////////
+
+// let str = 'innocent rabbit';
+
+// function checkSpam(str) {
+//     console.log(str)
+//   str.toLowerCase();
+//   console.log(str)
+//   if (str.includes("viagra") || str.includes("xxx")) {
+//     console.log("true");
+//   } else {
+//     console.log("false");
+//   }
+// }
+// checkSpam(str);
+
+////////////////////////////////////////////////
+
+//усечение строки///////////////////////////////
+
+// function truncate(str, maxlength) {
+//   if (str.length > maxlength) {
+//     return alert(str.slice(0, (maxlength - 1))+ "...");
+//   } else {
+//     return alert(str);
+//   }
+// }
+
+// truncate("Вот, что мне хотелось бы сказать на эту тему:", 30);
+
+////////////////////////////////////////////////
+
+//ВЫДЕЛИТЬ ЧИСЛО////////////////////////////////
+
+// function extractCurrencyValue(str) {
+//   return str.slice(1);
+// }
+// console.log(extractCurrencyValue("$120"));
+
+
+
+////////////////////////////////////////////////
