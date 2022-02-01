cb4bc6d02b4fe33a362a7db880bcd9eae5119745
Сергей Щуклин <wtorkanorka.shuclin@yandex.ru>
2022-02-01 17:35:22 +0500

    дз

--- script.js
+++ script.js
@@ -107,3 +107,24 @@
 
 //РАБОТАЕТ
 ////////////////////////////////////////////////
+
+// function truncate(str, maxlength) {
+//     if (str.length > maxlength){
+//     str = str.slice(0, maxlength - 1) + '…' ;
+//         if ((str.maxlength)=="," || (str.maxlength)=="." || (str.maxlength)==":" || (str.maxlength)==";") {
+//             // str = str.slice(0, maxlength - 1) + '…' ;
+//             str = str.slice(0, -1) + '...'
+//             return str;
+//         }
+//         else {
+//             return str;
+//         }
+//     }
+//     else{ return str;}
+//   }
+
+// let str = "1234,567"
+
+// alert(truncate(str, 6));
+
+///////////////////////////////////////////////
